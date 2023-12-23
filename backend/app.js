f18274334c8b7a7163bec4cc527e4784d715a079
MarMarcz <s24566@pjwstk.edu.pl>
2023-12-23 17:03:12 +0100

    added components to see coins and deleted useless files

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const getCoinsRouter = require('./endpoints/getCoins');
 const getCoinRouter = require('./endpoints/getCoin');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 databaseConnection();
 
