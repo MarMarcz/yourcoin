65c19dd1f0dc939fa7317bb9d9bad355f0406e95
MarMarcz <s24566@pjwstk.edu.pl>
2024-01-08 15:01:53 +0100

    fixed editReviewForm

--- frontend/app/components/AddCoinForm.tsx
+++ frontend/app/components/AddCoinForm.tsx
@@ -16,10 +16,11 @@ interface FormValues {
 const CoinAddSchema = Yup.object().shape({
   title: Yup.string()
     .transform(value => value.replace(/\s/g, ''))
+    .matches(/^[a-zA-Z0-9\s]*$/, 'cannot contain special characters')
     .required('Required'),
   image: Yup.string()
     .required('Required'),
-    prizeWithoutShipping: Yup.number()
+  prizeWithoutShipping: Yup.number()
     .min(100, 'Price cannot be less than 100')
     .max(10000, 'Price cannot be more than 10000')
     .required('Required'),
@@ -42,77 +43,86 @@ const CoinAddSchema = Yup.object().shape({
 });
 
 const AddCoinForm: React.FC = () => (
-  <div>
-    <h1>Add Coin</h1>
-    <Formik
-      initialValues={{
-        title: '',
-        image: '',
-        prizeWithoutShipping: 0,
-        prizeWithShipping: 0,
-        shortDescription: '',
-        quantityInStock: 0,
-        material: '',
-        extendedDescription: '',
-      }}
-      validationSchema={CoinAddSchema}
-      onSubmit={(values: FormValues, { resetForm }) => {
-        fetch('http://localhost:3001/api/addCoin', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(values)
-        })
-        .then(response => {
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-          return response.json();
-        })
-        .then(data => {
-          console.log(data);
-          alert('Form submitted successfully');
-          resetForm();
-        })
-        .catch((error) => {
-          console.error('Error:', error);
-        });
-      }}
-    >
-      {({ errors, touched }) => (
-        <Form style={{ display: 'flex', flexDirection: 'column', maxWidth: '300px', margin: '0 auto' }}>
-        <label>Title</label>
-        <Field name="title" style={{ marginBottom: '10px' }}/>
-        {errors.title && touched.title ? <div>{errors.title}</div> : null}
-        <label>Image</label>
-        <Field name="image" style={{ marginBottom: '10px' }}/>
-        {errors.image && touched.image ? <div>{errors.image}</div> : null}
-        <label>Price Without Shipping</label>
-        <Field name="prizeWithoutShipping" type="number" style={{ marginBottom: '10px' }}/>
-        {errors.prizeWithoutShipping && touched.prizeWithoutShipping ? <div>{errors.prizeWithoutShipping}</div> : null}
-        <label>Price With Shipping</label>
-        <Field name="prizeWithShipping" type="number" style={{ marginBottom: '10px' }}/>
-        {errors.prizeWithShipping && touched.prizeWithShipping ? <div>{errors.prizeWithShipping}</div> : null}
-        <label>Short Description</label>
-        <Field name="shortDescription" style={{ marginBottom: '10px' }}/>
-        {errors.shortDescription && touched.shortDescription ? <div>{errors.shortDescription}</div> : null}
-        <label>Quantity In Stock</label>
-        <Field name="quantityInStock" type="number" style={{ marginBottom: '10px' }}/>
-        {errors.quantityInStock && touched.quantityInStock ? <div>{errors.quantityInStock}</div> : null}
-        <label>Material</label>
-        <Field as="select" name="material" style={{ marginBottom: '10px' }}>
-        <option value="Gold">Gold</option>
-        <option value="Silver">Silver</option>
-        </Field>
-        {errors.material && touched.material ? <div>{errors.material}</div> : null}
-        <label>Extended Description</label>
-        <Field name="extendedDescription" style={{ marginBottom: '10px' }}/>
-        {errors.extendedDescription && touched.extendedDescription ? <div>{errors.extendedDescription}</div> : null}
-        <button type="submit">Submit</button>
-        </Form>
-      )}
-    </Formik>
+  <div className="flex flex-col items-center justify-center min-h-screen ">
+    <div className="w-full max-w-md p-8 m-4 bg-white rounded shadow-md">
+      <h1 className="mb-4 text-2xl font-bold text-center">Add Coin</h1>    <Formik
+        initialValues={{
+          title: '',
+          image: '',
+          prizeWithoutShipping: 0,
+          prizeWithShipping: 0,
+          shortDescription: '',
+          quantityInStock: 0,
+          material: '',
+          extendedDescription: '',
+        }}
+        validationSchema={CoinAddSchema}
+        onSubmit={(values: FormValues, { resetForm }) => {
+          fetch('http://localhost:3001/api/addCoin', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(values)
+          })
+            .then(response => {
+              if (!response.ok) {
+                throw new Error('Network response was not ok');
+              }
+              return response.json();
+            })
+            .then(data => {
+              console.log(data);
+              alert('Form submitted successfully');
+              resetForm();
+            })
+            .catch((error) => {
+              console.error('Error:', error);
+            });
+        }}
+      >
+        {({ errors, touched }) => (
+          <Form className="flex flex-col space-y-4">
+  <label className="font-bold">Title</label>
+  <Field name="title" className="px-3 py-2 border rounded-md"/>
+  {errors.title && touched.title ? <div className="text-red-500">{errors.title}</div> : null}
+
+  <label className="font-bold">Image</label>
+  <Field name="image" className="px-3 py-2 border rounded-md"/>
+  {errors.image && touched.image ? <div className="text-red-500">{errors.image}</div> : null}
+
+  <label className="font-bold">Price Without Shipping</label>
+  <Field name="prizeWithoutShipping" type="number" className="px-3 py-2 border rounded-md"/>
+  {errors.prizeWithoutShipping && touched.prizeWithoutShipping ? <div className="text-red-500">{errors.prizeWithoutShipping}</div> : null}
+
+  <label className="font-bold">Price With Shipping</label>
+  <Field name="prizeWithShipping" type="number" className="px-3 py-2 border rounded-md"/>
+  {errors.prizeWithShipping && touched.prizeWithShipping ? <div className="text-red-500">{errors.prizeWithShipping}</div> : null}
+
+  <label className="font-bold">Short Description</label>
+  <Field name="shortDescription" className="px-3 py-2 border rounded-md"/>
+  {errors.shortDescription && touched.shortDescription ? <div className="text-red-500">{errors.shortDescription}</div> : null}
+
+  <label className="font-bold">Quantity In Stock</label>
+  <Field name="quantityInStock" type="number" className="px-3 py-2 border rounded-md"/>
+  {errors.quantityInStock && touched.quantityInStock ? <div className="text-red-500">{errors.quantityInStock}</div> : null}
+
+  <label className="font-bold">Material</label>
+  <Field as="select" name="material" className="px-3 py-2 border rounded-md">
+    <option value="Gold">Gold</option>
+    <option value="Silver">Silver</option>
+  </Field>
+  {errors.material && touched.material ? <div className="text-red-500">{errors.material}</div> : null}
+
+  <label className="font-bold">Extended Description</label>
+  <Field name="extendedDescription" className="px-3 py-2 border rounded-md"/>
+  {errors.extendedDescription && touched.extendedDescription ? <div className="text-red-500">{errors.extendedDescription}</div> : null}
+
+  <button type="submit" className="px-3 py-2 mt-4 font-bold text-white bg-blue-500 rounded-md">Submit</button>
+</Form>
+        )}
+      </Formik>
+    </div>
   </div>
 );
 
--- frontend/app/components/EditReviewForm.tsx
+++ frontend/app/components/EditReviewForm.tsx
@@ -1,36 +1,42 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useLayoutEffect } from 'react';
 import axios from 'axios';
-import { Formik, Field, Form } from 'formik';
-import * as Yup from 'yup';
 
 interface Review {
+    _id: string;
     user: string;
     text: string;
     rating: number;
-    _id: {
-        $oid: string;
-      };
 }
 
 interface Coin {
-    _id: {
-        $oid: string;
-      };
-    title: string;
+    _id: string;
     reviews: Review[];
+    title: string;
 }
-
-const ReviewEditSchema = Yup.object().shape({
-    user: Yup.string().required('Required'),
-    text: Yup.string().required('Required'),
-    rating: Yup.number()
-        .min(1, 'Rating cannot be less than 1')
-        .max(5, 'Rating cannot be more than 5')
-        .required('Required'),
-});
 
 const EditReviewForm: React.FC = () => {
     const [coins, setCoins] = useState<Coin[]>([]);
+    const [activeInput, setActiveInput] = useState<string | null>(null);
+
+    useLayoutEffect(() => {
+        const inputElements = document.querySelectorAll('input');
+        inputElements.forEach(input => {
+            if (input.id === activeInput) {
+                input.style.backgroundColor = 'pink';
+            } else {
+                input.style.backgroundColor = '';
+            }
+        });
+    }, [activeInput]);
+
+    const fetchCoins = async () => {
+        try {
+            const response = await axios.get('http://localhost:3001/api/coins');
+            setCoins(response.data);
+        } catch (error) {
+            console.error('Error fetching coins:', error);
+        }
+    };
 
     useEffect(() => {
         const fetchData = async () => {
@@ -45,24 +51,37 @@ const EditReviewForm: React.FC = () => {
         fetchData();
     }, []);
 
+    useEffect(() => {
+        console.log('Coins:', coins);
+    }, [coins]);
+
     const handleInputChange = (coinId: string, reviewId: string, field: string, value: string | number) => {
+        if ((field === 'user' || field === 'text') && !/^[a-zA-Z0-9\s]*$/.test(value as string)) {
+            alert('No special characters allowed');
+            return;
+        }
+
+        if (field === 'rating' && ((value as number) < 1 || (value as number) > 5)) {
+            alert('Rating must be between 1 and 5');
+            return;
+        }
         setCoins((prevCoins) =>
             prevCoins.map((coin) => {
-                if (coin._id.$oid === coinId) {
+                if (coin._id === coinId) {
                     return {
                         ...coin,
                         reviews: coin.reviews.map((review) => {
-                            if (review._id.$oid === reviewId) {
+                            if (review._id === reviewId) {
                                 return {
                                     ...review,
                                     [field]: value,
                                 };
                             }
-                            return review;
+                            return { ...review };
                         }),
                     };
                 }
-                return coin;
+                return { ...coin };
             })
         );
     };
@@ -71,8 +90,8 @@ const EditReviewForm: React.FC = () => {
         const confirmed = window.confirm('Are you sure you want to update it?');
         if (confirmed) {
             try {
-                const coinToUpdate = coins.find((coin) => coin._id.$oid === coinId);
-                const reviewToUpdate = coinToUpdate?.reviews.find((review) => review._id.$oid === reviewId);
+                const coinToUpdate = coins.find((coin) => coin._id === coinId);
+                const reviewToUpdate = coinToUpdate?.reviews.find((review) => review._id === reviewId);
                 if (coinToUpdate && reviewToUpdate) {
                     const response = await axios.put(`http://localhost:3001/api/editOpinion/${coinId}/${reviewId}`, reviewToUpdate);
                     console.log('Data sent successfully:', response.data);
@@ -83,45 +102,78 @@ const EditReviewForm: React.FC = () => {
         }
     };
 
+    const handleDeleteClick = async (coinId: string, reviewId: string) => {
+        const confirmed = window.confirm('Are you sure you want to delete this review?');
+        if (confirmed) {
+            try {
+                await axios.delete(`http://localhost:3001/api/deleteOpinion/${coinId}/${reviewId}`);
+                fetchCoins();
+            } catch (error) {
+                console.error('Error deleting review:', error);
+            }
+        }
+    };
+
+    useEffect(() => {
+        fetchCoins();
+    }, []);
+
     return (
-        <div>
-            <h1>Edit Review</h1>
-            {coins.map((coin) =>
-                coin.reviews.map((review) => (
-                    <div key={review._id.$oid}>
-                        <h3>
-                            {coin.title}
-                            <button onClick={() => handleSendClick(coin._id.$oid, review._id.$oid)}>Send</button>
-                        </h3>
-                        <p>
-                            User:
-                            <input
-                                type="text"
-                                value={review.user}
-                                onChange={(e) => handleInputChange(coin._id.$oid, review._id.$oid, 'user', e.target.value)}
-                            />
-                        </p>
-                        <p>
-                            Text:
-                            <input
-                                type="text"
-                                value={review.text}
-                                onChange={(e) => handleInputChange(coin._id.$oid, review._id.$oid, 'text', e.target.value)}
-                            />
-                        </p>
-                        <p>
-                            Rating:
-                            <input
-                                type="number"
-                                value={review.rating}
-                                min={1}
-                                max={5}
-                                onChange={(e) => handleInputChange(coin._id.$oid, review._id.$oid, 'rating', Number(e.target.value))}
-                            />
-                        </p>
-                    </div>
-                ))
-            )}
+        <div className="flex flex-col items-center justify-center min-h-screen">
+            <div className="w-full max-w-md p-8 m-4 bg-white rounded shadow-md">
+                <h1 className="mb-4 text-2xl font-bold text-center">Edit Review</h1>
+                {coins.map((coin) =>
+                    coin.reviews.map((review) => (
+                        <div key={review._id} className="mb-4">
+                            <h3 className="flex justify-between">
+                                {coin.title}
+                                <div>
+                                    <button className="px-3 py-1 bg-green-500 text-white rounded mr-2" onClick={() => handleSendClick(coin._id, review._id)}>Send</button>
+                                    <button className="px-3 py-1 bg-red-500 text-white rounded" onClick={() => handleDeleteClick(coin._id, review._id)}>Delete</button>
+                                </div>
+                            </h3>
+                            <p>
+                                User:
+                                <input
+                                    id={`${coin._id}-${review._id}-user`}
+                                    type="text"
+                                    value={review.user}
+                                    onFocus={() => setActiveInput(`${coin._id}-${review._id}-user`)}
+                                    onBlur={() => setActiveInput(null)}
+                                    onChange={(e) => handleInputChange(coin._id, review._id, 'user', e.target.value)}
+                                    className="w-full px-3 py-2 mt-1 border rounded-md"
+                                />
+                            </p>
+                            <p>
+                                Text:
+                                <input
+                                    id={`${coin._id}-${review._id}-text`}
+                                    type="text"
+                                    value={review.text}
+                                    onFocus={() => setActiveInput(`${coin._id}-${review._id}-text`)}
+                                    onBlur={() => setActiveInput(null)}
+                                    onChange={(e) => handleInputChange(coin._id, review._id, 'text', e.target.value)}
+                                    className="w-full px-3 py-2 mt-1 border rounded-md"
+                                />
+                            </p>
+                            <p>
+                                Rating:
+                                <input
+                                    id={`${coin._id}-${review._id}-rating`}
+                                    type="number"
+                                    value={review.rating}
+                                    min={1}
+                                    max={5}
+                                    onFocus={() => setActiveInput(`${coin._id}-${review._id}-rating`)}
+                                    onBlur={() => setActiveInput(null)}
+                                    onChange={(e) => handleInputChange(coin._id, review._id, 'rating', Number(e.target.value))}
+                                    className="w-full px-3 py-2 mt-1 border rounded-md"
+                                />
+                            </p>
+                        </div>
+                    ))
+                )}
+            </div>
         </div>
     );
 };
